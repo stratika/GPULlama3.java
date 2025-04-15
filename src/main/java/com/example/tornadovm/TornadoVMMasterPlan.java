46a22a57b58d9a18744a92d0665b72a1b6810a19
mikepapadim <mikepapadim@hotmail.com>
2025-04-15 14:53:05 +0300

    More debug

--- src/main/java/com/example/tornadovm/TornadoVMMasterPlan.java
+++ src/main/java/com/example/tornadovm/TornadoVMMasterPlan.java
@@ -57,6 +57,32 @@ public class TornadoVMMasterPlan {
 
     }
 
+    public void tornadoVMForwardExecuteLayer(int position, int ll) {
+        int kvDim = (config.dim * config.numberOfKeyValueHeads) / config.numberOfHeads;
+
+        state.positionAndLayer.set(0, position);
+
+        // Update before execute (it an every copy in)
+        executionPlan.withGraph(0).withGridScheduler(scheduler).execute();
+
+        for (int l = 0; l < ll; l++) {
+            state.positionAndLayer.set(1, l);
+
+            int loff = l * config.contextLength * kvDim;
+            state.positionAndLayer.set(3, loff);
+            int layerOffsetForCaches =  loff + position * kvDim;
+            state.positionAndLayer.set(2, layerOffsetForCaches);
+
+            // Layer taskgraph
+            executionPlan.withGraph(1).withGridScheduler(scheduler).execute();
+        }
+
+        // Final RMSNorm and Logits
+        executionPlan.withGraph(2).withGridScheduler(scheduler).execute();
+
+    }
+
+
     private void logMemoryUsage(TornadoExecutionResult executionResult, String stage, int layer) {
         long totalDeviceMemoryUsage = executionResult.getProfilerResult().getTotalDeviceMemoryUsage();
         double memoryInMB = totalDeviceMemoryUsage / (1024.0 * 1024.0);
