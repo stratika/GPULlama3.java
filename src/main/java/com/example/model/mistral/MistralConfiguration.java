cd0feb8b376e266694eeab8465de08bfa5ebbc16
mikepapadim <mikepapadim@hotmail.com>
2025-08-01 14:53:19 +0300

    Refactor `MistralConfiguration` constructor to improve readability with multiline formatting

--- src/main/java/com/example/model/mistral/MistralConfiguration.java
+++ src/main/java/com/example/model/mistral/MistralConfiguration.java
@@ -2,8 +2,17 @@ package com.example.model.mistral;
 
 import com.example.model.Configuration;
 
-public record MistralConfiguration(int dim, int hiddenDim, int numberOfLayers, int numberOfHeads, int numberOfKeyValueHeads, int vocabularySize, int contextLength, boolean sharedWeights,
-                                   float rmsNormEps, float ropeTheta) implements Configuration {
+// @formatter:off
+public record MistralConfiguration(int dim,
+                                   int hiddenDim,
+                                   int numberOfLayers,
+                                   int numberOfHeads,
+                                   int numberOfKeyValueHeads,
+                                   int vocabularySize,
+                                   int contextLength,
+                                   boolean sharedWeights,
+                                   float rmsNormEps,
+                                   float ropeTheta) implements Configuration {
 
     public int kvDim() {
         return dim * numberOfKeyValueHeads / numberOfHeads;
